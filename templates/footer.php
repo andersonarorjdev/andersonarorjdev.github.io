31afa889668fba88d2468af7d4dc22b78441ac94
andersonjunior-vwi <andersonjunior@vivaweb.net>
2020-05-10 19:48:10 -0300

    Subindo a primeira versão beta do site para fins de teste

---
+++ templates/footer.php
@@ -0,0 +1,16 @@
+<div class="container-fluid fundo-rodape">
+    <div class="row py-3">
+    <div class="col-lg-6 d-flex justify-content-start">
+            <span class="text-white font-weight-bold mt-lg-2 align-items-sm-start fonte-rodape">
+                ©2020 MEASY COPORATION ALLRIGHTS RESERVED®
+            </span>
+        </div>
+        <div class="col-lg-6 d-flex justify-content-md-end d-flex justify-content-sm-start">
+          <a href="https://www.instagram.com/measy/" class="text-decoration-none" target="blank"><img class="ml-2 ml-lg-4" src="./imagens/brands-and-logotypes.png" width="35px" height="35px" alt=""></a>
+          <a href=""><img class="ml-3 ml-lg-4" src="./imagens/social-2.png" width="35px" height="35px" alt=""></a>
+          <a href=""><img class="ml-3 ml-lg-4" src="./imagens/social-media-logo.png" width="35px" height="35px" alt=""></a>
+          <a href=""><img class="ml-3 ml-lg-4" src="./imagens/social-media.png" width="35px" height="35px" alt=""></a>
+          <a href=""><img class="ml-3 ml-lg-4" src="./imagens/social.png" width="35px" height="35px" alt=""></a>
+        </div>
+    </div>
+</div>
---
+++ templates/nav.php
@@ -0,0 +1,50 @@
+<!--Inicio do navbar-->
+<nav class="navbar navbar-expand-lg sticky-top navbar-dark fundo-navbar">
+  <a class="navbar-brand ml- pr-3" href="#"><img class="ml-3" src="./imagens/logo.png" height="45px" alt=""></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="menu">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link ml-3" href="#"><span class="text-danger fontes-navbar"><img src="./imagens/corneta.png" width="26px" height="26px" alt=""> ANUNCIANTE</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link ml-3" href="#"><span class="text-warning fontes-navbar"><img src="./imagens/technology.png" width="26px" height="26px" alt=""> INFLUENCIADOR</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link  ml-3" href="#"><span class="text-success fontes-navbar"><img src="./imagens/investidor-2.png" width="26px" height="26px" alt=""> INVESTIDOR</span></a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <button class="btn btn-dark my-2 my-sm-0 mr-2 border border-white fontes-navbar" type="submit"><img src="./imagens/seo-and-web.png" width="26px" height="26px" alt=""> HOMEBROKER</button>
+      <button class="btn btn-outline-info my-2 my-sm-0 fontes-navbar" type="button" data-toggle="modal" data-target="#login"><img src="./imagens/multimedia.png" width="26px" height="26px" alt=""> LOGIN</button>
+    </form>
+  </div>
+</nav>
+<!--fim do navbar-->
+
+<!--inicio do modal-->
+<div class="modal fade mt-5" id="login" tabindex="-1" role="dialog" data-backdrop="static" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+          <div class="modal-content">
+           <div class="modal-header">
+             <h5 class="modal-title" id="modal"><span class="text-info font-weight-bold"><img src="./imagens/gamer.png" width="50px" height="50px" alt=""> VOCÊ AINDA NÃO É UM MEASYER!</span></h5>
+               <a class="close" data-dismiss="modal" aria-label="Close">
+                 <span aria-hidden="true"><img src="./imagens/signs.png" width="30px" height="30px" alt=""></span>
+              </a>
+            </div>
+            <div class="modal-body fundo-navbar">
+                <div class=" mt-2">
+                  <p class="text-white texto-modal p-3 font-weight-bold">
+                    Entre em contato com <a class="text-decorarion-underline text-success" href="">influenciadores@imeasy.com.br</a>,  conheça e faça parte do mundo mágico da influência!
+                  </p>
+                </div>
+            </div>
+            <div class="modal-footer">
+              <a type="button" class="btn btn-outline-success" href="mailto:influenciadores@imeasy.com?Subject=Hello%20again" target="blank"><img src="./imagens/interface.png" width="30px" height="30px" alt=""> Entrar em contato</a>
+          </div>
+        </div>
+      </div>
+    </div>
+<!--fim do modal-->
