711801b7cfa1c005309ccb98496582fead86026f
andersonarorjdev <anderson.r.o.r.jr@gmail.com>
2020-11-27 13:01:34 -0300

    Desenolvido a parte da navbar responsiva com o menu já responsivo.

---
+++ javascript/Menu.js
@@ -0,0 +1,9 @@
+const OpenMenu = _ =>{
+ let ResponsiveMenu = document.getElementById('ResponsiveMenu');
+  ResponsiveMenu.style.display = "block";
+}
+
+const CloseMenu = _ =>{
+  let ResponsiveMenu = document.getElementById('ResponsiveMenu');
+  ResponsiveMenu.style.display = "none";
+}
