b376300ff9586a6dd0bda1abae0507bef344506b
andersonarorjdev <anderson.r.o.r.jr@gmail.com>
2020-11-28 13:52:06 -0300

    Desenvolvido o slider de whybuy mobile first.

--- javascript/MainSlider.js
+++ javascript/MainSlider.js
@@ -1,6 +1,9 @@
 const NextSlide = _ =>{
   let Slidder = document.getElementById('MainSlider');
+  let Radio1 = document.getElementById('Radio1');
+  let Radio2 = document.getElementById('Radio2');
       Slidder.style.backgroundImage = "url('../images/RunnerModal.svg')";
+      Radio2.style.backgroundColor = "#FAA500";
 }
 
 const PreviousSlide = _ =>{
