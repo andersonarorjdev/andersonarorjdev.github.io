9d9709800118eb6028394f272879cd81fd8ddefd
andersonarorjdev <anderson.r.o.r.jr@gmail.com>
2021-01-07 10:18:05 -0300

    Concerted the problem with navbar bugging.

--- Backend/Locate.js
+++ Backend/Locate.js
@@ -1,25 +1,23 @@
 const RecuperaPosicao = _ =>{
     if (navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(showPosition);
+      navigator.geolocation.getCurrentPosition(showPosition, ErrorTakePosition);
           }
       else{x.innerHTML="O seu navegador não suporta Geolocalização.";}
+      }
+
+      const ErrorTakePosition = error =>{
+          if(error.PERMISSION_DENIED){
+            console.log('Recusou!');
+          }
       }
       const showPosition = position =>{
           let latitude = position.coords.latitude;
           let longitude = position.coords.longitude;
-
-          //Creating element to show message: "Loading"
-          let y = document.createElement('img');
-            y.src = 'location.gif';
-            y.style.height = '400px';
-            y.style.width = '200px';
-
-            let tag = document.getElementById('x');
-  
+          
           axios({
               url:
-                `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}&zoom=18&addressdetails=1`
-                ,
+              `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}&zoom=18&addressdetails=1`
+              ,
               method:'get',
               data:{
                   format: 'json',
@@ -27,30 +25,30 @@ const RecuperaPosicao = _ =>{
                   lon:longitude,
                   addressdetails:1,
                   'accept-language':'pt-BR'
-                  }        
-              },
-                tag.appendChild(y)
-                )
-              .then((response) => {
-                  let state = response.data.address.state;
-                  let city = null;
-
-                    if(response.data.address.town == undefined){
-                       city = response.data.address.city;   
-                    }
-                    else{
-                        city = response.data.address.town;
-                    }
-
-                  let street = response.data.address.road;
-                  let StateSymbol = null;
-                    console.log('Ok')
-                      console.log(state, city, street);
-                      console.log(response);
-
-                    if(state == 'Acre'){
-                        StateSymbol = 'AC';
-                    }if(state == 'Alagoas'){
+                }        
+            },
+            document.getElementById('LOADING').style.display = 'flex'
+            )
+            .then((response) => {
+                let state = response.data.address.state;
+                let city = null;
+                
+                if(response.data.address.town == undefined){
+                    city = response.data.address.city;   
+                }
+                else{
+                    city = response.data.address.town;
+                }
+                
+                let street = response.data.address.road;
+                let StateSymbol = null;
+                console.log('Ok')
+                console.log(state, city, street);
+                console.log(response);
+                
+                if(state == 'Acre'){
+                    StateSymbol = 'AC';
+                }if(state == 'Alagoas'){
                         StateSymbol = 'AL';
                     }if(state == 'Amapá' || state == 'Amapa'){
                         StateSymbol = 'AP';
@@ -105,19 +103,28 @@ const RecuperaPosicao = _ =>{
                     }else{
                         console.log('Localização não encontrada!');
                     }
-
+                    
                     axios({
-                            url: `https://viacep.com.br/ws/${StateSymbol}/${city}/${street}/json`,
-                            method: 'get',
-                        })
-                        .then((response) =>{
+                        url: `https://viacep.com.br/ws/${StateSymbol}/${city}/${street}/json`,
+                        method: 'get',
+                    })
+                    .then((response) =>{
                         console.log(response);
                         let cep = response.data[0].cep;
                         console.log(cep);
-                        x.innerHTML = `Seu CEP é : ${cep}, cidade: ${city}, estado: ${state}`;
-                            })
-                     
-                  
-              })
-  
+
+                        let CEPTAG = document.getElementById('CEP');
+                        let STREETAG = document.getElementById('STREET');
+                        let CITYTAG = document.getElementById('CITY');
+                        let LOADINGIF = document.getElementById('LOADING');
+
+                        LOADINGIF.style.display = 'none';
+                        CEPTAG.innerHTML = cep;
+                        STREETAG.innerHTML = street;
+                        CITYTAG.innerHTML = city;
+                    })
+                    
+                    
+                })
+                
   }
