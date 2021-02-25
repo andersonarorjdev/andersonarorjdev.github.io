7554596aed6a1cadd76a2030722f7f345e93725a
andersonarorjdev <anderson.r.o.r.jr@gmail.com>
2021-02-25 00:34:36 -0300

    Finished responsived contact page

--- src/pages/Contact/styled-Contact.jsx
+++ src/pages/Contact/styled-Contact.jsx
@@ -2,8 +2,13 @@ import styled from 'styled-components';
 
 export const ContactContainer = styled.section`
     height:100%;
+    max-height:99vh;
 
     display:flex;
     justify-content:center;
     align-items:center;
+
+    @media(max-width:800px){
+        flex-direction:column;
+    }
 `;
